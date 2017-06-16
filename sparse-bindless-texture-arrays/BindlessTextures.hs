2638014fea6b54ccb41801ded868f1d6185a0959
Oliver Charles <ollie@ocharles.org.uk>
2017-06-16 09:52:20 +0100

    Use sparse textures

--- sparse-bindless-texture-arrays/BindlessTextures.hs
+++ sparse-bindless-texture-arrays/BindlessTextures.hs
@@ -17,10 +17,13 @@ import qualified Data.Vector.Storable as Vector
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
-import Foreign.Ptr (castPtr, nullPtr)
+import Foreign.Ptr (Ptr, castPtr, nullPtr)
+import Text.Printf
 import Foreign.Storable (peek, sizeOf)
 import Graphics.GL.Core45
 import Graphics.GL.Ext.ARB.BindlessTexture
+import Graphics.GL.Ext.ARB.DebugOutput
+import Graphics.GL.Ext.ARB.SparseTexture
 import Graphics.GL.Types
 import qualified SDL
 import System.IO (stdout)
@@ -34,6 +37,11 @@ main = runManaged $ do
   unless
     gl_ARB_bindless_texture
     (error "Your graphic card does not support ARB_bindless_texture")
+
+  -- We need ARB_sparse_texture for spare textures
+  unless
+    gl_ARB_sparse_texture
+    (error "Your graphic card does not support ARB_sparse_texture")
 
   -- Upload a texture to OpenGL. Uses Juicy Pixels to load from disk, and then
   -- glGenTextures/glTexStorage2D/glTexSubImage2D to create and upload the data
@@ -74,12 +82,15 @@ main = runManaged $ do
 
     SDL.glSwapWindow window
 
+    liftIO $ threadDelay 1000000
+
 
 -- createOpenGLWindow :: (MonadIO m) => m SDL.Window
 createOpenGLWindow = do
   SDL.initialize [ SDL.InitVideo ]
   window <- managed (bracket createGLWindow SDL.destroyWindow)
   SDL.glCreateContext window >>= SDL.glMakeCurrent window
+  installDebugHook
   return window
 
   where
@@ -173,13 +184,19 @@ uploadRGB8 (Image width height pixels) = liftIO $ do
   name <- alloca $ \namePtr -> do
     glGenTextures 1 namePtr
     peek namePtr
+
   glBindTexture GL_TEXTURE_2D name
+
+  -- Mark the texture as being sparse
+  glTexParameteri GL_TEXTURE_2D GL_TEXTURE_SPARSE_ARB GL_TRUE
+
   glTexStorage2D
     GL_TEXTURE_2D
     1
     GL_RGB8
     (fromIntegral width)
     (fromIntegral height)
+
   Vector.unsafeWith pixels $ \imageData ->
     glTexSubImage2D
       GL_TEXTURE_2D
@@ -191,4 +208,75 @@ uploadRGB8 (Image width height pixels) = liftIO $ do
       GL_RGB
       GL_UNSIGNED_BYTE
       (castPtr imageData)
+
+  glTexPageCommitmentARB
+    GL_TEXTURE_2D
+    0
+    0
+    0
+    0
+    (fromIntegral width)
+    (fromIntegral height)
+    0
+    GL_TRUE
+
   return name
+
+
+-- | This will install a synchronous debugging hook to allow OpenGL to notify us
+-- if we're doing anything deprecated, non-portable, undefined, etc.
+installDebugHook :: MonadIO m => m ()
+installDebugHook
+  | gl_ARB_debug_output = do
+    cb <- liftIO $ mkGLDEBUGPROC glCallback
+    glDebugMessageCallbackARB cb nullPtr
+    glEnable GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB
+  | otherwise = return ()
+
+
+glCallback :: GLenum
+           -> GLenum
+           -> GLuint
+           -> GLenum
+           -> GLsizei
+           -> Ptr GLchar
+           -> Ptr ()
+           -> IO ()
+glCallback source t ident severity _ message _ = do
+  message' <- peekCString message
+  putStrLn $
+    printf
+      "opengl %s [%s] %s (%s): %s"
+      t'
+      severity'
+      source'
+      (show ident)
+      message'
+  where
+    source' :: String
+    source' =
+      case source of
+        GL_DEBUG_SOURCE_API_ARB -> "API"
+        GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB -> "Window System"
+        GL_DEBUG_SOURCE_SHADER_COMPILER_ARB -> "Shader Compiler"
+        GL_DEBUG_SOURCE_THIRD_PARTY_ARB -> "Third Party"
+        GL_DEBUG_SOURCE_APPLICATION_ARB -> "Application"
+        GL_DEBUG_SOURCE_OTHER_ARB -> "Other"
+        _ -> "Unknown"
+    t' :: String
+    t' =
+      case t of
+        GL_DEBUG_TYPE_ERROR_ARB -> "Error"
+        GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB -> "Deprecated Behaviour"
+        GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB -> "Undefined Behaviour"
+        GL_DEBUG_TYPE_PORTABILITY_ARB -> "Portability"
+        GL_DEBUG_TYPE_PERFORMANCE_ARB -> "Performance"
+        GL_DEBUG_TYPE_OTHER_ARB -> "Other"
+        _ -> "Unknown"
+    severity' :: String
+    severity' =
+      case severity of
+        GL_DEBUG_SEVERITY_HIGH_ARB -> "High"
+        GL_DEBUG_SEVERITY_MEDIUM_ARB -> "Medium"
+        GL_DEBUG_SEVERITY_LOW_ARB -> "Low"
+        _ -> "Unknown"
